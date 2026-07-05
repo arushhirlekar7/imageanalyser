aff83196b9faf3ee9345bcb333d93b04a40a11f0
arushhirlekar7 <arushhirlekar7@gmail.com>
2026-07-05 20:46:08 +0530

    Add files via upload

---
+++ streamlit_images_demo.py
@@ -0,0 +1,39 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama import ChatOllama
+import base64
+import os
+import streamlit as st
+
+
+def encode_image(uploaded_file):
+    return base64.b64encode(uploaded_file.read()).decode()
+
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+llm = ChatOllama(model="llava")
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant that can describe images."),
+        (
+            "human",
+            [
+                {"type": "text", "text": "{input}"},
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": "data:image/jpeg;base64,""{image}",
+                        "detail": "low",
+                    },
+                },
+            ],
+        ),
+    ]
+)
+chain=prompt|llm
+uploaded_file=st.file_uploader("Upload your image",type=["jpg","png"])
+question=st.text_input("Enter a question")
+if question:
+    image=encode_image(uploaded_file)
+    response=chain.invoke({"input" : question,"image":image})
+    st.write(response.content)
